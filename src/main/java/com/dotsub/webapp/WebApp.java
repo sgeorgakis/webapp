979d0028c168693e6580fed5f8ea6b3fa8502666
Stefanos Georgakis <s.georgakis@chq.jrtechnologies.com>
2018-05-18 10:32:12 +0300

    Add checkstyle and jacoco test coverage report

--- src/main/java/com/dotsub/webapp/WebApp.java
+++ src/main/java/com/dotsub/webapp/WebApp.java
@@ -1,13 +1,24 @@
 package com.dotsub.webapp;
 
+import com.dotsub.webapp.config.ApplicationProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
 @EnableAutoConfiguration
+@EnableConfigurationProperties(ApplicationProperties.class)
 public class WebApp {
 
+    private WebApp() {
+    }
+
+    /**
+     * Main method, used to run the application.
+     *
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
         SpringApplication.run(WebApp.class, args);
     }
