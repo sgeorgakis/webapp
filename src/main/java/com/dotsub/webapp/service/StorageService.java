6b75ea3e011426a0d168dcf660238893c678386d
Stefanos Georgakis <s.georgakis@chq.jrtechnologies.com>
2018-05-18 11:35:33 +0300

    Implement storage service

--- src/main/java/com/dotsub/webapp/service/StorageService.java
+++ src/main/java/com/dotsub/webapp/service/StorageService.java
@@ -2,9 +2,11 @@ package com.dotsub.webapp.service;
 
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+
 public interface StorageService {
 
-    String save(MultipartFile file);
+    String save(MultipartFile file) throws IOException;
 
     boolean delete(String path);
 }
--- src/main/java/com/dotsub/webapp/service/impl/LocalStorageService.java
+++ src/main/java/com/dotsub/webapp/service/impl/LocalStorageService.java
@@ -1,21 +1,60 @@
 package com.dotsub.webapp.service.impl;
 
+import com.dotsub.webapp.config.ApplicationProperties;
 import com.dotsub.webapp.service.StorageService;
+import org.apache.commons.io.FilenameUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 
 @Service
 public class LocalStorageService implements StorageService {
 
-    //TODO: implement methods
+    private final ApplicationProperties applicationProperties;
 
-    @Override
-    public String save(MultipartFile file) {
-        return null;
+    public LocalStorageService(ApplicationProperties applicationProperties) {
+        this.applicationProperties = applicationProperties;
     }
 
+    /**
+     * Save the file to the specified folder
+     * and return its full path
+     *
+     * @param file the file to save
+     * @return the path of the saved file
+     * @throws IOException
+     */
+    @Override
+    public String save(MultipartFile file) throws IOException {
+        OutputStream outputStream = null;
+        try {
+            String fullName = file.getOriginalFilename();
+            String path = applicationProperties.getUpload().getSaveFolder();
+            File targetFile = new File(FilenameUtils.concat(path, fullName));
+            outputStream = new FileOutputStream(targetFile);
+            outputStream.write(file.getBytes());
+            return targetFile.getPath();
+        } finally {
+            if (outputStream != null) {
+                outputStream.close();
+            }
+        }
+    }
+
+    /**
+     * Delete the file in the path
+     *
+     * @param path the path of the file
+     * @return true if the file was deleted, false if not
+     */
     @Override
     public boolean delete(String path) {
-        return false;
+        File targetFile = new File(path);
+        return targetFile.delete();
     }
 }
