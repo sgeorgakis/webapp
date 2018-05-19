2b55c35b7f1bd091ec4ac9347f5006bd76b9557f
Stefanos Georgakis <s.georgakis@chq.jrtechnologies.com>
2018-05-19 09:39:22 +0300

    Add view class

---
+++ src/main/java/com/dotsub/webapp/web/rest/view/FileDataView.java
@@ -0,0 +1,47 @@
+package com.dotsub.webapp.web.rest.view;
+
+import java.time.Instant;
+
+public class FileDataView {
+
+    private Long id;
+
+    private Instant creationDate;
+
+    private String title;
+
+    private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Instant getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Instant creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+}
