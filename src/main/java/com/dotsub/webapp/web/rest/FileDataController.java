e7348ef50315b997c3e0d976ed3258caec62e34e
Stefanos Georgakis <s_georgakis@hotmail.com>
2018-05-29 22:21:10 +0300

    Remove cors annotation from controller

--- src/main/java/com/dotsub/webapp/web/rest/FileDataController.java
+++ src/main/java/com/dotsub/webapp/web/rest/FileDataController.java
@@ -7,7 +7,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,7 +21,6 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "http://localhost:4200")
 public class FileDataController {
 
     private final FileDataMapper mapper;
