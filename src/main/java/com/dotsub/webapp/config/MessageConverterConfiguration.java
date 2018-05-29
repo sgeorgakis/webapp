4d531396c141915d46abeb0d0e63d3b557daa343
Stefanos Georgakis <s_georgakis@hotmail.com>
2018-05-30 00:08:32 +0300

    Fix Jackson2HttpMessage converter

--- src/main/java/com/dotsub/webapp/config/MessageConverterConfiguration.java
+++ src/main/java/com/dotsub/webapp/config/MessageConverterConfiguration.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
@@ -22,12 +21,12 @@ public class MessageConverterConfiguration implements WebMvcConfigurer {
 
     /**
      * Create a jackson to http message converter and use the application property file
-     * to enable/disable various properties
+     * to enable/disable various properties.
+     * The default one must be removed in order to register the new one.
      *
-     * @return
      */
-    @Bean
-    MappingJackson2HttpMessageConverter jackson2HttpMessageConverter() {
+    @Override
+    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
         MappingJackson2HttpMessageConverter jsonConverter = new MappingJackson2HttpMessageConverter();
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.enable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
@@ -35,12 +34,7 @@ public class MessageConverterConfiguration implements WebMvcConfigurer {
         objectMapper.disable(SerializationFeature.WRITE_DATE_TIMESTAMPS_AS_NANOSECONDS);
         objectMapper.disable(DeserializationFeature.READ_DATE_TIMESTAMPS_AS_NANOSECONDS);
         jsonConverter.setObjectMapper(objectMapper);
-        return jsonConverter;
-    }
-
-    @Override
-    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
-
-        converters.add(jackson2HttpMessageConverter());
+        converters.removeIf(c -> c instanceof MappingJackson2HttpMessageConverter);
+        converters.add(jsonConverter);
     }
 }
