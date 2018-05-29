240c4d8ce2fd540e1a75e0760eba8c6df2012fd5
Stefanos Georgakis <s_georgakis@hotmail.com>
2018-05-29 22:19:12 +0300

    Finalize tests

--- src/test/java/com/dotsub/webapp/web/rest/FileDataControllerTest.java
+++ src/test/java/com/dotsub/webapp/web/rest/FileDataControllerTest.java
@@ -2,26 +2,30 @@ package com.dotsub.webapp.web.rest;
 
 import com.dotsub.webapp.WebApp;
 import com.dotsub.webapp.service.FileDataService;
+import com.dotsub.webapp.service.StorageService;
 import com.dotsub.webapp.service.mapper.FileDataMapper;
+import org.junit.After;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.mock.web.MockPart;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.io.File;
-import java.nio.ByteBuffer;
-import java.nio.charset.Charset;
-import java.nio.file.Files;
-import java.nio.file.attribute.UserDefinedFileAttributeView;
+import java.io.InputStream;
 
+import static com.dotsub.webapp.config.Constants.ErrorCode.FILE_ALREADY_EXISTS_ERROR;
+import static com.dotsub.webapp.config.Constants.ErrorCode.INVALID_ARGUMENTS_ERROR;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE;
+import static org.springframework.http.MediaType.MULTIPART_FORM_DATA;
 import static org.springframework.http.MediaType.MULTIPART_FORM_DATA_VALUE;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -31,9 +35,18 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest(classes = WebApp.class)
 public class FileDataControllerTest {
 
+    private static final String TITLE_REQUEST_PART = "title";
+
+    private static final String DESCRIPTION_REQUEST_PART = "description";
+
+    private static final String FILE_REQUEST_PART = "file";
+
     private static final String TITLE_VALUE = "testTitle";
 
     private static final String DESCRIPTION_VALUE = "testDescription";
+
+    private static final String FILE = "image.jpeg";
+
     @Autowired
     private FileDataMapper fileDataMapper;
 
@@ -41,7 +54,13 @@ public class FileDataControllerTest {
     private FileDataService fileDataService;
 
     @Autowired
+    private StorageService storageService;
+
+    @Autowired
     private HttpMessageConverter[] httpMessageConverters;
+
+    @Autowired
+    private ResponseEntityExceptionHandler exceptionHandler;
 
     private MockMvc restMvc;
 
@@ -54,29 +73,92 @@ public class FileDataControllerTest {
 
         this.restMvc = MockMvcBuilders.standaloneSetup(fileDataController)
                 .setMessageConverters(httpMessageConverters)
+                .setControllerAdvice(exceptionHandler)
                 .build();
     }
 
-    @Ignore
-    @Before
-    public void addAttributes() throws Exception {
-        ClassLoader classLoader = getClass().getClassLoader();
-        file = new File(classLoader.getResource("image.jpeg").getFile());
-        Files.setAttribute(file.toPath(), "user:title", Charset.defaultCharset().encode(TITLE_VALUE));
-        Files.setAttribute(file.toPath(), "user:description", Charset.defaultCharset().encode(DESCRIPTION_VALUE));
-    }
-
-    @Ignore
     @Test
     public void uploadFile() throws Exception {
-        MockMultipartFile multipartFile = new MockMultipartFile("file", file.getName(), MULTIPART_FORM_DATA_VALUE, Files.readAllBytes(file.toPath()));
+        ClassLoader classLoader = getClass().getClassLoader();
+        try (InputStream file = classLoader.getResourceAsStream(FILE)) {
+
+            MockMultipartFile multipartFile = new MockMultipartFile(FILE_REQUEST_PART, FILE, MULTIPART_FORM_DATA_VALUE, file);
+            assertThat(multipartFile).isNotNull();
+
+            MockPart title = new MockPart(TITLE_REQUEST_PART, TITLE_REQUEST_PART, TITLE_VALUE.getBytes());
+            title.getHeaders().setContentType(MULTIPART_FORM_DATA);
+
+            MockPart description = new MockPart(DESCRIPTION_REQUEST_PART, DESCRIPTION_REQUEST_PART, DESCRIPTION_VALUE.getBytes());
+            description.getHeaders().setContentType(MULTIPART_FORM_DATA);
+
+            restMvc.perform(multipart("/api/file-data/upload")
+                    .file(multipartFile)
+                    .part(title, description)
+                    .accept(APPLICATION_JSON_UTF8_VALUE))
+                    .andExpect(status().isCreated())
+                    .andExpect(jsonPath("$.id").exists())
+                    .andExpect(jsonPath("$.title").value(TITLE_VALUE))
+                    .andExpect(jsonPath("$.description").value(DESCRIPTION_VALUE))
+                    .andExpect(jsonPath("$.creationDate").exists());
+        }
+    }
+
+    @Test
+    public void uploadWithoutTitle() throws Exception {
+
+        ClassLoader classLoader = getClass().getClassLoader();
+        try (InputStream file = classLoader.getResourceAsStream(FILE)) {
+
+        MockMultipartFile multipartFile = new MockMultipartFile(FILE_REQUEST_PART, FILE, MULTIPART_FORM_DATA_VALUE, file);
         assertThat(multipartFile).isNotNull();
 
-        restMvc.perform(multipart("/api/files/upload")
+        MockPart description = new MockPart(DESCRIPTION_REQUEST_PART, DESCRIPTION_REQUEST_PART, DESCRIPTION_VALUE.getBytes());
+        description.getHeaders().setContentType(MULTIPART_FORM_DATA);
+
+        restMvc.perform(multipart("/api/file-data/upload")
                 .file(multipartFile)
-                .accept(MULTIPART_FORM_DATA_VALUE))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.title").value(TITLE_VALUE))
-                .andExpect(jsonPath("$.description").value(DESCRIPTION_VALUE));
+                .part(description)
+                .accept(APPLICATION_JSON_UTF8_VALUE))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.timestamp").exists())
+                .andExpect(jsonPath("$.code").value(INVALID_ARGUMENTS_ERROR.getCode()))
+                .andExpect(jsonPath("$.message").value(INVALID_ARGUMENTS_ERROR.getMessage()));
+        }
+    }
+
+    @Test
+    public void uploadExistingFile() throws Exception {
+        ClassLoader classLoader = getClass().getClassLoader();
+        try (InputStream file = classLoader.getResourceAsStream(FILE)) {
+
+            MockMultipartFile multipartFile = new MockMultipartFile(FILE_REQUEST_PART, FILE, MULTIPART_FORM_DATA_VALUE, file);
+            assertThat(multipartFile).isNotNull();
+
+            fileDataService.save(multipartFile, TITLE_VALUE, DESCRIPTION_VALUE);
+
+            MockPart title = new MockPart(TITLE_REQUEST_PART, TITLE_REQUEST_PART, TITLE_VALUE.getBytes());
+            title.getHeaders().setContentType(MULTIPART_FORM_DATA);
+
+            MockPart description = new MockPart(DESCRIPTION_REQUEST_PART, DESCRIPTION_REQUEST_PART, DESCRIPTION_VALUE.getBytes());
+            description.getHeaders().setContentType(MULTIPART_FORM_DATA);
+
+            restMvc.perform(multipart("/api/file-data/upload")
+                    .file(multipartFile)
+                    .part(title, description)
+                    .accept(APPLICATION_JSON_UTF8_VALUE))
+                    .andExpect(status().isBadRequest())
+                    .andExpect(jsonPath("$.timestamp").exists())
+                    .andExpect(jsonPath("$.code").value(FILE_ALREADY_EXISTS_ERROR.getCode()))
+                    .andExpect(jsonPath("$.message").value(FILE_ALREADY_EXISTS_ERROR.getMessage()));
+        }
+    }
+
+    @After
+    public void cleanUp() {
+        fileDataService.findAll()
+                .forEach(f -> {
+                    storageService.delete(f.getPath());
+                    fileDataService.delete(f.getId());
+                });
     }
 }
